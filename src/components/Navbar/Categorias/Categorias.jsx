f978d7b7a2d0727af38ee83b646dad9fe6195dad
Julian Barberis <juli_barberis@hotmail.com>
2022-11-24 19:43:29 -0300

    PreEntrega1_Barberis

---
+++ src/components/Navbar/Categorias/Categorias.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Categorias = () => {
+    return (
+        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+                <a className="nav-link" href="#">Inicio</a>
+            </li>
+            <li className="nav-item">
+                <a className="nav-link" href="#">Monitores</a>
+            </li>
+            <li className="nav-item">
+                <a className="nav-link" href="#">Componentes</a>
+            </li>
+            <li className="nav-item">
+                <a className="nav-link" href="#">Perifericos</a>
+            </li>
+      </ul>
+    );
+}
+
+export default Categorias;
